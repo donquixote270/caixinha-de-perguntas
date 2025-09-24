26a8fcb778e32a9f357c136c698528f373979070
donquixote270 <martins.pereira.victor@escola.pr.gov.br>
2025-09-24 11:05:55 -0300

    .

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 function criarCartão(categoria, pergunta, resposta) {
-let container = document.getElementById('container')
-let cartão = document.createElement('article')
-cartão.className = ('cartão')
-cartão.innerHTML = <div class="cartao_conteudo">
-                    <h3>the new order: Alemanha</h3>
-                    <div class="cartao_conteudo_pergunta">
-                        <P>qual é o grande plano do goring?</P>
-                    </div>
-                    <div class="cartao_conteudo_resposta">
-                       <p>dominação mundial por guerra.</p>
-                    </div>
-                </div>
+    let container = document.getElementById('container')
+    let cartão = document.createElement('article')
+    cartão.className = ('cartão')
+    cartão.innerHTML = <div class="cartao_conteudo">
+        <h3>the new order: Alemanha</h3>
+        <div class="cartao_conteudo_pergunta">
+            <P>qual é o grande plano do goring?</P>
+        </div>
+        <div class="cartao_conteudo_resposta">
+            <p>dominação mundial por guerra.</p>
+        </div>
+    </div>
 }
